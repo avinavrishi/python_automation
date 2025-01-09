c232834d529af23596c139a516f60e46a2e7de3e
avinavrishi <arishi560@gmail.com>
2025-01-10 01:41:37 +0530

    Upgraded Algorithm

--- Excersice/exsecise_8_jan.py
+++ Excersice/exsecise_8_jan.py
@@ -7,14 +7,26 @@ Write a function to count how many students are "Present" in the class.'''
 #     for i in range(0, len(attendance_list)):
 #         if ((attendance_list[i])[1])=="Present":
 #             count+=1
+    
+    
+#     # Upgrade algorithm 2
+#     for i in attendance_list:
+#         if i[1] == "Present":
+#             count+=1
+
+#     # Upgrade algorithm 2
+#     data = sum([1 for i in attendance_list if i[1] == "Present"])
+
 #     return(count)
 
-# print()
 
 # attendance = [("Alice", "Present"), ("Bob", "Absent"), ("Charlie", "Present")]
 # present_count=int(present_student(attendance))
 # print(f"Total present students are {present_count}")
 # print()
+
+#-----------------------------------------------------------------------------------------------------
+#-----------------------------------------------------------------------------------------------------
 
 ''' A company needs to validate email addresses. Write a function that checks if an email is valid.
 Rules for validity : The email must contain "@" and ".".
@@ -22,17 +34,25 @@ Return "Valid Email" if both conditions are met. Otherwise, return "Invalid Emai
 '''
 
 # def email(email):
-#     if email=="":
-#         print("please Enter valid Email ID")
-#     elif "@" in email and "." in email:
-#          print("Valid Email ID")
-#     else:
-#         print("Invalid Email ID")
+#     if len(email)==0 or " " in email:
+#         return "Invalid Email ID"
+
+#     if email.count("@") == 1:
+#         if "@" in email and "." in email:
+#             return "Valid Email ID"
+#         else:
+#             return "Invalid Email ID"
+
+#     # Upgrade Algorithm 2
+#     validate_email = "Valid Email" if '@' in email and '.' in email else "Invalid Email" if len(email)==0 or " " in email else "Invalid Email"
+    
+#     return "Invalid Email ID"
+
+
     
 # print()
 # userEmail=input("Enter valid Email ID : ")
-# email(userEmail)
-# print()
+# print(email(userEmail))
 
 '''A grocery store stores its inventory as a list of tuples:
 inventory = [("Apples", 50), ("Bananas", 20), ("Oranges", 0)]
@@ -41,24 +61,20 @@ The function should return:
 "In Stock" if the quantity is greater than 0.
 "Out of Stock" otherwise.
 # '''
-# def checkitem(item_to_check):
-#     is_in_stock=False
+
+# def checkitem(item_to_check, inventory):
 #     for i in range(len(inventory)):   
 #        if item_to_check in inventory[i]:
-#         is_in_stock=True
 #         item=inventory[i]
 #         if item[1]>0:
-#            print("Item is in stock")
+#            return "Item is in stock"
 #         else:
-#            print("Item is out of stock")      
+#            return "Item is out of stock"      
 
-#     if is_in_stock==False:
-#        print("Please enter valid item")          
-# print()
-# inventory=[("Apples", 50), ("Bananas", 20), ("Oranges", 0)]
-# item=input("Enter the item you want to buy : \n")
-# checkitem(item)
-# print()
+#     return "Please enter valid item"          
+
+# print(checkitem(input("Enter the item you want to buy : \n"), [("Apples", 50), ("Bananas", 20), ("Oranges", 0)]))
+
 
 '''An e-commerce system stores items in a cart as a list of tuples:
 cart = [("Laptop", 1, 800), ("Mouse", 2, 20), ("Keyboard", 1, 50)]
@@ -119,53 +135,53 @@ if it is "Available". If the book is already borrowed, return "Book is already b
 A function return_book(book_name) that changes the status of a book to "Available". 
 If the book is not borrowed, return "Book is already available."'''
 
-# def list_books(status):
-#      book_list=[]
-#      for i in range(0,len(books)):
-#           if (books[i])[1]==status:
-#               book_list.append(books[i])
-#      return(book_list) 
+def list_books(status):
+     book_list=[]
+     for i in range(0,len(books)):
+          if (books[i])[1]==status:
+              book_list.append(books[i])
+     return(book_list) 
     
-# def borrow_book(book_name):
-#      new_list=[]
-#      for i in range(0,len(books)):
-#           if(books[i])[0]==book_name:
-#            if (books[i])[1]=="Available":
-#                new_list=list(books[i])
-#                new_list[1]="Borrowed"
-#                books[i]=new_list
-#                print(books)
-#            else:
-#                print("Book is already borrowed.")               
+def borrow_book(book_name):
+     new_list=[]
+     for i in range(0,len(books)):
+          if(books[i])[0]==book_name:
+           if (books[i])[1]=="Available":
+               new_list=list(books[i])
+               new_list[1]="Borrowed"
+               books[i]=new_list
+               print(books)
+           else:
+               print("Book is already borrowed.")               
             
-# def return_book(book_name):
-#      new_list=[]
-#      for i in range(0,len(books)):
-#           if(books[i])[0]==book_name:
-#            if (books[i])[1]=="Borrowed":
-#                new_list=list(books[i])
-#                new_list[1]="Available"
-#                books[i]=new_list
-#                print(books)
-#            else:
-#                print("Book is available.")               
+def return_book(book_name):
+     new_list=[]
+     for i in range(0,len(books)):
+          if(books[i])[0]==book_name:
+           if (books[i])[1]=="Borrowed":
+               new_list=list(books[i])
+               new_list[1]="Available"
+               books[i]=new_list
+               print(books)
+           else:
+               print("Book is available.")               
 
-# print()
-# books = [("Book A", "Available"), ("Book B", "Borrowed"), ("Book C", "Available")]
-# status_of_book=input("Available or Borrowed : \n")
-# available_books_list=[list_books(status_of_book)]
-# print(f"List of {status_of_book} books is {available_books_list}")
-# print()
-# print("#########################################################")
-# print()
-# book1=input("Enter the book name you want to borrow : \n")
-# borrow_book(book1)
-# print()
-# print("#########################################################")
-# print()
-# book2=input("Enter the book name to return: \n")
-# return_book(book2)
-# print()
+print()
+books = [("Book A", "Available"), ("Book B", "Borrowed"), ("Book C", "Available")]
+status_of_book=input("Available or Borrowed : \n")
+available_books_list=[list_books(status_of_book)]
+print(f"List of {status_of_book} books is {available_books_list}")
+print()
+print("#########################################################")
+print()
+book1=input("Enter the book name you want to borrow : \n")
+borrow_book(book1)
+print()
+print("#########################################################")
+print()
+book2=input("Enter the book name to return: \n")
+return_book(book2)
+print()
 
 '''7. An event organizer tracks registrations as a list of tuples:
 registrations = [("Alice", "Paid"), ("Bob", "Not Paid"), ("Charlie", "Paid")]
@@ -180,46 +196,43 @@ If the participant does not exist, return "Participant not found."
 A function count_status() that returns a dictionary with the count of "Paid" 
 and "Not Paid" participants.'''
 
-def get_paid_registrations(parti_list):
-    for i in range(0, len(parti_list)):
-        if (parti_list[i])[1]=="Paid":
-            paid_list.append((parti_list[i])[0])        
-    return (paid_list)
+# def get_paid_registrations(parti_list):
+#     for i in range(0, len(parti_list)):
+#         if (parti_list[i])[1]=="Paid":
+#             paid_list.append((parti_list[i])[0])        
+#     return (paid_list)
 
 
-def mark_paid(name):
-    is_paid=False
-    is_available=True
-    for i in range(0,len(registrations)):
-        if (registrations[i])[0]==name and (registrations[i][1]=="Not Paid"):
-            is_paid=True
-            my_list=list(registrations[i])
-            my_list[1]="Paid"
-            registrations[i]=my_list
-            print(registrations)
-        elif (registrations[i])[0]!=name:
-            is_available=False
+# def mark_paid(name):
+#     is_paid=False
+#     is_available=True
+#     for i in range(0,len(registrations)):
+#         if (registrations[i])[0]==name and (registrations[i][1]=="Not Paid"):
+#             is_paid=True
+#             my_list=list(registrations[i])
+#             my_list[1]="Paid"
+#             registrations[i]=my_list
+#             print(registrations)
+#         elif (registrations[i])[0]!=name:
+#             is_available=False
     
-    if is_paid==False:   
-        print("Already Paid")
+#     if is_paid==False:   
+#         print("Already Paid")
 
-    if is_available==False:   
-        print("Participant not found.")
+#     if is_available==False:   
+#         print("Participant not found.")
     
-            
-
 
 # def count_status():
+# print()
+# registrations = [("Alice", "Paid"), ("Bob", "Not Paid"), ("Charlie", "Paid")]
+# paid_list=[]
+# paid_list=get_paid_registrations(registrations)
+# print(f"The list of all paid participants is {paid_list}")
+# print()
+# print("#########################################################")
+# print()
+# paid_name=input("Enter the name who paid the fees : ")
+# mark_paid(paid_name)
 
-print()
-registrations = [("Alice", "Paid"), ("Bob", "Not Paid"), ("Charlie", "Paid")]
-paid_list=[]
-paid_list=get_paid_registrations(registrations)
-print(f"The list of all paid participants is {paid_list}")
-print()
-print("#########################################################")
-print()
-paid_name=input("Enter the name who paid the fees : ")
-mark_paid(paid_name)
-
-print()
+# print()
