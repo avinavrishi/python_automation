e16352056d33eef2fc26c27aacb2f8c8c002d02c
avinavrishi <arishi560@gmail.com>
2025-01-17 01:24:31 +0530

    Module and git ignore file

---
+++ Classes/modules.py
@@ -0,0 +1,15 @@
+# Topic Modules
+
+import oops as my_file
+
+p2 = my_file.Person("Suraj", 15)
+p2.print_message()
+
+
+To find the directory of the module
+x = dir(oops)
+print(x)
+
+from oops import print_message
+print_message()
+
