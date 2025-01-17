100c91665c08d4876a54da59675264bdb77ca246
SayaliAutade <sayalidengale@gmail.com>
2025-01-17 10:00:36 -0800

    commit_17Jan

--- Excersice/excersice_oops.py
+++ Excersice/excersice_oops.py
@@ -133,3 +133,79 @@ Print the final details of the product.'''
 # p1.sell(7)
 # p1.change_price(1200)
 # p1.final_details()
+
+'''University Enrollment System
+Create a class University with:
+
+Class variables: max_courses_per_student (default 5) and total_students_enrolled.
+Instance attributes: student_name, student_id, and enrolled_courses (a list of course names).
+Tasks:
+
+Write a method to enroll a student in a course. If the maximum course limit is reached,   
+display an error.
+Add a method to drop a course by its name.
+Implement a class method to change the max_courses_per_student limit.
+Create a method to generate a report of all students and their enrolled courses 
+(use a class-level dictionary to store student data).
+Add a method to calculate the average number of courses enrolled per student.
+Constraints:
+
+Ensure total_students_enrolled is updated whenever a new student is created.
+Prevent duplicate course enrollments for the same student.'''
+
+class University():
+#     max_courses_per_student={"Science":20,"Commerce":25,"Art":30,"Dairy Science":15,"ITI":40}
+    max_courses_per_student=[] 
+    total_students_enrolled=[]
+    
+    def __init__(self,student_name,student_id,enrolled_courses):
+        self.student_name
+        self.student_id
+        self.enrolled_courses=[] 
+    
+    def enroll_student_course(self,student_name,enrolled_courses):
+        
+        
+
+
+
+
+# u1=University(student_name,student_id,enrolled_courses)
+
+
+'''Online Shopping Cart System
+Create a class ShoppingCart that simulates a shopping cart system for an e-commerce platform. 
+The class has:
+
+A class variable discount_rate representing a global discount applied to all carts (default 0%).
+Instance attributes for cart_id (unique), items (dictionary with item name as the key and a 
+tuple of price and quantity as the value), and total.
+Tasks:
+
+Create a method to add an item to the cart. If the item already exists, update its quantity.
+
+Implement a method to remove an item by name. If the item doesn't exist, handle gracefully.
+
+Write a method to calculate the total price after applying the class-level discount_rate.
+
+Add a method to update the global discount_rate for all carts.
+
+Create a class method to generate a summary of all carts (use a class variable all_carts to
+track all created carts).
+
+Constraints:
+
+Ensure discount_rate applies uniformly to all carts.
+Handle edge cases like removing an item not in the cart or setting a negative discount rate.'''
+
+class ShoppingCart():
+    discount_rate=0
+
+    def __init__(self,cart_id,items,total):
+        self.cart_id=cart_id
+        self.items=items
+        self.total=total
+    
+    def add_item(self,):
+
+s1=ShoppingCart(10,{"Body Soap":(20,5), "Shampoo":(150,2),"Toothpaste":(45,2)},490)
